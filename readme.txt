288d55ea7f5c2d3dc49c39fde5ad861fa6b0929f
Amanda Chan <ac2921@princeton.edu>
2025-04-04 15:03:45 -0400

    tried to fix database error handling

---
+++ readme.txt
@@ -0,0 +1,60 @@
+Readme File: 
+----------------------------------------------------------------------------
+Amanda Chan (ac2921)
+Arnold Jiang (aj9183)
+----------------------------------------------------------------------------
+Partner 1:
+----------------------------------------------------------------------------
+Amanda: I w
+
+I worked on the basic framework for the html files. I also wrote 
+all of the database.py logic by incorporating the sql logic from assignment
+1. On the database.py, I also reformatted the return output so that it can be
+processed by reg.py. I also wrote all of the Flask logic in reg.py, 
+including using the flask.request.args.get methods, calling the database 
+functions, calling the flask.render_template methods. I also implemented 
+all of the cookies logic in reg.py. Finally, I wrote the runserver.py main
+method. 
+ 
+---------------------------------------------------------------------------
+Partner 2:
+----------------------------------------------------------------------------
+Arnold: 
+
+I helped Amanda with creating the regdetails and regoverviews html files. 
+Specifically, I wrote the html code for adding inline CSS styling using HTML
+attributes. I integrated the Flask and Django code to work concurrently with
+the SQL querying. I added the testing modules to the reg.py. I worked on 
+the error handling portion of the project. I also created the testing 
+modules for the reg.py file. I also resolved the issues with the cookie 
+and worked on the "another class search" feature. I resolved an issue with
+the server not properly running.
+
+----------------------------------------------------------------------------
+Help Received:
+----------------------------------------------------------------------------
+Help we received from other people: 
+
+Ambri OH (3/28)
+Jocelyn OH (3/28)
+Jocelyn OH (3/29)
+Xinran OH (3/31)
+
+----------------------------------------------------------------------------
+Time Spent: 
+----------------------------------------------------------------------------
+Time spent doing the assignment: 12 hours
+
+----------------------------------------------------------------------------
+Citations:
+----------------------------------------------------------------------------
+Sources of Information: EdStem, Lecture handouts, course page
+
+----------------------------------------------------------------------------
+Learnings:
+----------------------------------------------------------------------------
+The assignment helped us gain a better understanding of how to deliver
+and run an application using python through routing and basic http requests,
+all of which were new concepts to us. We also gained a better understanding
+of using Django and Flask as well as writing HTML for the first time, which
+helped our understanding of how to develop front end code.
--- reg.py
+++ reg.py
@@ -29,38 +29,56 @@ def reg_overviews():
         "title": flask.request.args.get("title")
     }
 
-    # Convert the result from database files as json
-    valid, result = database.reg_overviews(query)
-    # ASK QUESTION HERE:
-    # The result is not what it is supposed to be for error handling. I would ask about this in OH
-    # THIS ISSUE IS PRESENT IN reg_details() too.
-    if not valid:
-        json_doc = json.dumps([valid, result])
+    try:
+        # Convert the result from database files as json
+        valid, result = database.reg_overviews(query)
+        # ASK QUESTION HERE:
+        # The result is not what it is supposed to be for error handling. I would ask about this in OH
+        # THIS ISSUE IS PRESENT IN reg_details() too.
+        if not valid:
+            json_doc = json.dumps([valid, result])
+            response = flask.make_response(json_doc)
+            response.headers['Content-Type'] = 'application/json'
+            return response
+        
+        json_doc = json.dumps(result)
         response = flask.make_response(json_doc)
         response.headers['Content-Type'] = 'application/json'
         return response
-    
-    json_doc = json.dumps(result)
-    response = flask.make_response(json_doc)
-    response.headers['Content-Type'] = 'application/json'
-    return response
-    
+    except Exception as e:
+        json_doc = json.dumps([False,
+"A server error occurred. Please contact the system administrator."])
+        response = flask.make_response(json_doc)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
 #-----------------------------------------------------------------------
 @app.route('/regdetails', methods=['GET'])
 def reg_details():
 
     query = {"classid": flask.request.args.get("classid")}
-    valid, result = database.reg_details(query)
-    if not valid:
-        json_doc = json.dumps([valid, result])
+    try:
+        valid, result = database.reg_details(query)
+        if not valid:
+            json_doc = json.dumps([valid, result])
+            response = flask.make_response(json_doc)
+            response.headers['Content-Type'] = 'application/json'
+            return response
+
+        json_doc = json.dumps(result)
+        response = flask.make_response(json_doc)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    
+    except Exception as e:
+        json_doc = json.dumps([False,
+"A server error occurred. Please contact the system administrator."])
         response = flask.make_response(json_doc)
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    json_doc = json.dumps(result)
-    response = flask.make_response(json_doc)
-    response.headers['Content-Type'] = 'application/json'
-    return response
+
+
 
 #-----------------------------------------------------------------------
 # For testing:
